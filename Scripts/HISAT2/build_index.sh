c0d9e8dc5613f3289b29147db8765ba87532b568
Bernadette Johnson <46578401+BernadetteBiology@users.noreply.github.com>
2026-03-03 19:52:27 -0800

    Add HISAT2 index building script
    This script builds an index for HISAT2 using a specified genome file.

---
+++ Scripts/HISAT2/build_index.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Building an Index for HISAT2
+
+#Make a new folder your HISAT2 index
+mkdir ~/03_hisat2/hisat2_index 
+
+#Change directory into folder
+cd ~/03_hisat2/hisat2_index
+
+#Check that your genome is located in the correct path
+hisat2-build ~/genome/*.fna genome_index &>> HISAT2_build.log
